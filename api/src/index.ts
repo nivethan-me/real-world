64ba9f44dbffbf40f3076b4e8a1eba84ca117127
nivethan <63112138+nivethan-me@users.noreply.github.com>
2025-05-12 00:04:22 +0530

    feat(api): add express server setup with nodejs watch mode

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('Welcome to the Real World API!');
+});
+
+// prettier-ignore
+app.listen(3001, () => {
+  console.log('\x1b[1;32m%s\x1b[0m', '┌──────────────────────────────────────────────────────────┐');
+  console.log('\x1b[1;32m%s\x1b[0m', '│         RUNNING REAL WORLD API SERVER                    │');
+  console.log('\x1b[1;32m%s\x1b[0m', '└──────────────────────────────────────────────────────────┘');
+  console.log('\x1b[1;32m%s\x1b[0m', '┌──────────────────────────────────────────────────────────┐');
+  console.log('\x1b[32m%s\x1b[0m',   '│         API:  http://localhost:3001/api                  │');
+  console.log('\x1b[1;32m%s\x1b[0m', '└──────────────────────────────────────────────────────────┘');
+});
